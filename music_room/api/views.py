c20bb907a3c6302e982327c2f4c234a5c073f400
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-08-10 16:40:05 +0300

    added: Room View

--- music_room/api/views.py
+++ music_room/api/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+
+from .serializers import RoomSerializer
+from .models import Room
 
 # Create your views here.
 # our end point for the music room app
-def main(request):
-    return HttpResponse("Hello, world. You're at the music room app main page.")
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
