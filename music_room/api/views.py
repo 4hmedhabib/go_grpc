5b93a728b1aedca16df16d84f4901124026854de
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-08-10 16:24:56 +0300

    added: main view set

--- music_room/api/views.py
+++ music_room/api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# our end point for the music room app
+def main(request):
+    return HttpResponse("Hello, world. You're at the music room app main page.")
