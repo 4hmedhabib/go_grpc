1b33916e3c4a8fca5ef32c0b0ea72bd757f7ec79
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-08-13 15:01:03 +0300

    updated: app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-import {Home} from './components'
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home, CreateRoom,  RoomJoin } from "./components";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <Home />
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/join" element={<RoomJoin />} />
+      <Route path="/create" element={<CreateRoom />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
