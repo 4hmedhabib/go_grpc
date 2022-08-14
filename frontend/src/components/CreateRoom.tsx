61c2ff3a51aa80058f2a6e590c1485f39cf56218
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-08-14 14:48:00 +0300

    added: creating post room

--- frontend/src/components/CreateRoom.tsx
+++ frontend/src/components/CreateRoom.tsx
@@ -1,4 +1,5 @@
-import React, { ChangeEvent, FormEvent, useState } from "react";
+import axios from "axios";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 type Props = {};
 
@@ -16,9 +17,15 @@ const CreateRoom = (props: Props) => {
     return;
   };
 
-  const handleRoomCreate = (e: FormEvent<HTMLFormElement>)  => {
-    
-  }
+  const handleRoomCreate = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const request = axios.post('/api/create-room/', {
+      votes_to_skip: votesToSkip,
+      guest_can_pause: guestCanPause
+    }).then(response => {
+      console.log(response.data)
+    }).catch(error => { console.log(error) });
+  };
 
   return (
     <div className="w-full flex flex-col items-center">
@@ -26,46 +33,52 @@ const CreateRoom = (props: Props) => {
       <p className="text-sm text-gray-500 tracking-wide font-mono">
         Guest Control of Playback State
       </p>
-      <div className="flex space-x-10 font-mono font-semibold">
-        <div className="flex flex-col items-center justify-center my-5 space-y-2">
-          <input
-            type="radio"
-            name="play/pause"
-            id="play/pause"
-            value="true"
-            className="checked:accent-blue-500"
-            onChange={(e) => handleGuestCanPauseChange(e)}
-          />
-          <label htmlFor="play/pause">Play/Pause</label>
+      <form onSubmit={handleRoomCreate}>
+        <div className="flex space-x-10 font-mono font-semibold">
+          <div className="flex flex-col items-center justify-center my-5 space-y-2">
+            <input
+              type="radio"
+              name="play/pause"
+              id="play/pause"
+              value="true"
+              className="checked:accent-blue-500"
+              onChange={(e) => handleGuestCanPauseChange(e)}
+            />
+            <label htmlFor="play/pause">Play/Pause</label>
+          </div>
+          <div className="flex flex-col items-center justify-center my-5 space-y-2">
+            <input
+              type="radio"
+              name="play/pause"
+              value="false"
+              id="no-control"
+              className="checked:accent-red-500"
+              onChange={(e) => handleGuestCanPauseChange(e)}
+            />
+            <label htmlFor="no-control">No Control</label>
+          </div>
         </div>
-        <div className="flex flex-col items-center justify-center my-5 space-y-2">
+        <div className="flex items-center justify-center">
           <input
-            type="radio"
-            name="play/pause"
-            value="false"
-            id="no-control"
-            className="checked:accent-red-500"
-            onChange={(e) => handleGuestCanPauseChange(e)}
+            type="number"
+            min={1}
+            name="votes"
+            id="votes"
+            step={1}
+            value={votesToSkip}
+            className="border-b-2 text-center pb-1 font-bold font-mono text-lg outline-none text-gray-800"
+            onChange={(e) => handleVotesToSkipChange(e)}
           />
-          <label htmlFor="no-control">No Control</label>
         </div>
-      </div>
-      <div className="flex items-center justify-center">
-        <input
-          type="number"
-          min={1}
-          name="votes"
-          id="votes"
-          step={1}
-          value={votesToSkip}
-          className="border-b-2 text-center pb-1 font-bold font-mono text-lg outline-none text-gray-800"
-          onChange={(e) => handleVotesToSkipChange(e)}
-        />
-      </div>
-      <div className="flex my-5 font-bold font-mono text-white space-x-5">
-        <button className="bg-blue-500 p-2 rounded-md">Create Room</button>
-        <button className="bg-red-500 py-2 px-5 rounded-md">Back</button>
-      </div>
+        <div className="flex my-5 font-bold font-mono text-white space-x-5">
+          <button type="submit" className="bg-blue-500 p-2 rounded-md">
+            Create Room
+          </button>
+          <button type="button" className="bg-red-500 py-2 px-5 rounded-md">
+            Back
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
