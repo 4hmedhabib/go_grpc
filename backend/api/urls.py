957a25775bb8677c56320cc85efdf98beec86c88
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-08-14 17:26:39 +0300

    added: get room api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.RoomView.as_view()),
-    path("create-room/", views.CreateRoomView.as_view(), name='create-room'),
+    path("room/", views.RoomView.as_view()),
+    path("create-room/", views.CreateRoomView.as_view(), name="create-room"),
+    path("get-room/", views.GetRoom.as_view(), name="get-room"),
 ]
