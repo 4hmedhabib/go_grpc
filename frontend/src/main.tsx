26d04f6b6eb2d8bfcadd4456be0aafb7e249b273
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-08-14 14:48:12 +0300

    added: axios config and lib

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://127.0.0.1:8000";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
