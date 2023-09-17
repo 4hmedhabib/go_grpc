f7154708d9c09187a11401b04a0a14675ac835c0
4hmedhabib <info.ahmed.developer@gmail.com>
2023-09-17 15:02:04 +0300

    updated: client, server and proto

--- client/main.go
+++ client/main.go
@@ -1,1 +1,29 @@
 package client
+
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	//client := pb.NewGreetServiceClient(conn)
+	//
+	//names := &pb.NameList{
+	//	Names: []string{"Ahmed", "Habib", "Hassan"},
+	//}
+	//
+	//callSayHello(client)
+}
--- server/main.go
+++ server/main.go
@@ -1,1 +1,29 @@
-package server
+package main
+
+import (
+	pb "github.com/4hmedhabib/go_grpc/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+const (
+	port = ":8080"
+)
+
+type helloServer struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to start the server %v", err)
+	}
+	gRPCServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(gRPCServer, &helloServer{})
+	log.Printf("Server started at %v", lis.Addr())
+	if err := gRPCServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to start: %v", err)
+	}
+}
